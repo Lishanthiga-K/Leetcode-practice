26cd94e598297dcaeadb5a665597a4bbb7aac53c
Lishanthiga-K <150430410+Lishanthiga-K@users.noreply.github.com>
2025-06-04 16:14:50 +0530

    Update Search2dMatrix.java

--- Search2dMatrix.java
+++ Search2dMatrix.java
@@ -2,14 +2,31 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-                else{
-                    continue;
-                }
+        int top = 0, bottom = m-1;
+        while(top<=bottom){
+            int mid = (top+bottom)/2;
+            if(matrix[mid][0]<target && matrix[mid][matrix[mid].length-1]>target){
+                break;
+            }
+            else if(matrix[mid][0]>target){
+                bottom = mid-1;
+            }
+            else{
+                top = mid + 1;
+            }
+        }
+        int row = (top+bottom)/2;
+        int left = 0,right=matrix[row].length-1;
+        while(left<=right){
+            int mid = (left + right)/2;
+            if(matrix[row][mid] == target){
+                return true;
+            }
+            else if(matrix[row][mid]<target){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
             }
         }
         return false;
